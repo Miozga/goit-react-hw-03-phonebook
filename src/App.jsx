80ef47ac31668178be4075fa8bb24f20199127ce
Sebastian Miozga <miozga.sebastian@gmail.com>
2024-05-24 16:27:00 +0200

    123

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,19 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = (name, number) => {
     const isExistingContact = this.state.contacts.some(
       contact => contact.name.toLowerCase() === name.toLowerCase()
